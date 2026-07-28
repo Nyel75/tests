5c97925d454e08fe893fbc37651cf3fd7bab43e2
Nyel75 <renniellomibao7@gmail.com>
2026-07-28 09:12:52 +0800

    change VUs to 10 and 180 total user

--- load.js
+++ load.js
@@ -1,21 +1,17 @@
 import http from 'k6/http';
-import { check, sleep } from 'k6';
+import { check } from 'k6';
 
 export const options = {
-  stages: [
-    { duration: '30s', target: 10 }, // Ramp up to 10 users
-    { duration: '1m', target: 10 },   // Stay at 10 users
-    { duration: '30s', target: 0 },   // Ramp down
-  ],
+  vus: 10,          // 10 users at the same time
+  iterations: 30,   // 30 iterations total
 
   thresholds: {
-    http_req_failed: ['rate<0.01'],      // Less than 1% failures
-    http_req_duration: ['p(95)<1000'],   // 95% requests < 1 second
+    http_req_failed: ['rate<0.01'],
+    http_req_duration: ['p(95)<1000'],
   },
 };
 
 const BASE_URL = 'http://172.20.0.121:8001';
-
 const TOKEN = 'f172782e-e202-486b-a714-35ce673eb61c';
 
 const params = {
@@ -26,22 +22,19 @@ const params = {
 };
 
 export default function () {
-
   const responses = http.batch([
     ['GET', `${BASE_URL}/api/template-manager/dl-types`, null, params],
     ['GET', `${BASE_URL}/api/template-manager/templates`, null, params],
     ['GET', `${BASE_URL}/api/template-manager/clients`, null, params],
     ['GET', `${BASE_URL}/api/template-manager/activity-logs`, null, params],
-    ['GET', `${BASE_URL}/api/template-manager/test-values`, null, params],
+    // Add the required query parameters below
+    ['GET', `${BASE_URL}/api/template-manager/test-values?folder_name=YOUR_FOLDER&template_name=YOUR_TEMPLATE&template_type=YOUR_TYPE`, null, params],
     ['GET', `${BASE_URL}/api/template-manager/existing-names`, null, params],
   ]);
 
   responses.forEach((res) => {
     check(res, {
       'status is 200': (r) => r.status === 200,
-      'response time < 1000ms': (r) => r.timings.duration < 1000,
     });
   });
-
-  sleep(1);
 }
